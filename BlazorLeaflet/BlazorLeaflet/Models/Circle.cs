e1b50fda4b6e9b82c450ef0927fefd6ff7a1545a
Mihai Stan <stanmihai17cs@gmail.com>
2019-09-23 17:50:15 +0200

    Add shapes data models

---
+++ BlazorLeaflet/BlazorLeaflet/Models/Circle.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorLeaflet.Models
+{
+    public class Circle : Path
+    {
+
+        /// <summary>
+        /// Radius of the circle, in meters.
+        /// </summary>
+        public double Radius { get; set; }
+
+    }
+}
