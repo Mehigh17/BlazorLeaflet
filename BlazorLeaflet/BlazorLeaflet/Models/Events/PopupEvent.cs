1ca99f6879e09e5904c35aec94b7c8f29bee97b2
Struemmeck <struemmeck@hotmail.com>
2020-01-19 10:47:39 +0100

    Add standard layer event types.

---
+++ BlazorLeaflet/BlazorLeaflet/Models/Events/PopupEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorLeaflet.Models.Events
+{
+    public class PopupEvent : Event
+    {
+        public Popup Popup { get; set; }
+    }
+}
---
+++ BlazorLeaflet/BlazorLeaflet/Models/Events/TooltipEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorLeaflet.Models.Events
+{
+    public class TooltipEvent : Event
+    {
+        public Tooltip Tooltip { get; set; }
+    }
+}
