edce7de105266e6f5a2d569e4c340b1fb84645bb
Struemmeck <struemmeck@hotmail.com>
2020-01-19 11:00:18 +0100

    Rearange Layer event handling properties and methods

--- BlazorLeaflet/BlazorLeaflet/Models/Layer.cs
+++ BlazorLeaflet/BlazorLeaflet/Models/Layer.cs
@@ -40,21 +40,7 @@ namespace BlazorLeaflet.Models
 
         public delegate void EventHandler(object sender, Event e);
 
-        public delegate void PopupEventHandler(object sender, PopupEvent e);
-
-        public delegate void TooltipEventHandler(object sender, TooltipEvent e);
-
         public event EventHandler OnAdd;
-
-        public event EventHandler OnRemove;
-
-        public event PopupEventHandler OnPopupOpen;
-
-        public event PopupEventHandler OnPopupClose;
-
-        public event TooltipEventHandler OnTooltipOpen;
-
-        public event TooltipEventHandler OnTooltipClose;
 
         [JSInvokable]
         public void NotifyAdd(Event eventArgs)
@@ -62,11 +48,17 @@ namespace BlazorLeaflet.Models
             OnAdd?.Invoke(this, eventArgs);
         }
 
+        public event EventHandler OnRemove;
+
         [JSInvokable]
         public void NotifyRemove(Event eventArgs)
         {
             OnRemove?.Invoke(this, eventArgs);
         }
+
+        public delegate void PopupEventHandler(object sender, PopupEvent e);
+
+        public event PopupEventHandler OnPopupOpen;
 
         [JSInvokable]
         public void NotifyPopupOpen(PopupEvent eventArgs)
@@ -74,17 +66,25 @@ namespace BlazorLeaflet.Models
             OnPopupOpen?.Invoke(this, eventArgs);
         }
 
+        public event PopupEventHandler OnPopupClose;
+
         [JSInvokable]
         public void NotifyPopupClose(PopupEvent eventArgs)
         {
             OnPopupClose?.Invoke(this, eventArgs);
         }
 
+        public delegate void TooltipEventHandler(object sender, TooltipEvent e);
+
+        public event TooltipEventHandler OnTooltipOpen;
+
         [JSInvokable]
         public void NotifyTooltipOpen(TooltipEvent eventArgs)
         {
             OnTooltipOpen?.Invoke(this, eventArgs);
         }
+
+        public event TooltipEventHandler OnTooltipClose;
 
         [JSInvokable]
         public void NotifyTooltipClose(TooltipEvent eventArgs)
