c229355e111641d1bfc526e63b3f8141ee448e19
Struemmeck <struemmeck@hotmail.com>
2020-01-24 10:05:38 +0100

    Add ToString override for LatLng for easier console debugging.

--- BlazorLeaflet/BlazorLeaflet/Models/LatLng.cs
+++ BlazorLeaflet/BlazorLeaflet/Models/LatLng.cs
@@ -11,5 +11,10 @@ namespace BlazorLeaflet.Models
         public decimal Lng { get; set; }
 
         public decimal Alt { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" (Lat: {Lat}, Lng: {Lng}, Alt: {Alt})";
+        }
     }
 }
