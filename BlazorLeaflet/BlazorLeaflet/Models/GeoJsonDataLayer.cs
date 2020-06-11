b0e971158e17adbafd3e0d6a527ea6860437ed48
Sören Kuklau <kuklau@trenz.de>
2020-06-11 12:26:02 +0200

    GeoJsonDataLayer: expose the properties

--- BlazorLeaflet/BlazorLeaflet/Models/GeoJsonDataLayer.cs
+++ BlazorLeaflet/BlazorLeaflet/Models/GeoJsonDataLayer.cs
@@ -12,17 +12,23 @@ namespace BlazorLeaflet.Models
         public bool HasPointToLayerFunc => PointToLayer != null;
 
         [System.Text.Json.Serialization.JsonIgnore]
-        public Func<object, LatLng, Marker> PointToLayer { get; set; }
+        public Func<GeoJsonFeature, LatLng, Marker> PointToLayer { get; set; }
 
         [JSInvokable]
-        public object CallPointToLayer(object point, LatLng latLng)
+        public object CallPointToLayer(GeoJsonFeature feature, LatLng latLng)
         {
-            var layer = PointToLayer(point, latLng);
+            var layer = PointToLayer(feature, latLng);
 
             if (!(layer is Marker))
                 throw new NotSupportedException("Only Marker layers are currently implemented for PointToLayer.");
 
             return new { Layer = layer, Ref = DotNetObjectReference.Create(layer) };
         }
+    }
+
+    public class GeoJsonFeature
+    {
+        public object Geometry { get; set; }
+        public Dictionary<string, object> Properties { get; set; }
     }
 }
