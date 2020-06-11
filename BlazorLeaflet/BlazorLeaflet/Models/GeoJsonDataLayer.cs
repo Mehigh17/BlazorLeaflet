3ac304307b0ee1723f7d288572a5ddc58af6c2f3
Sören Kuklau <kuklau@trenz.de>
2020-06-11 09:52:15 +0200

    GeoJsonDataLayer optionally supports pointToLayer

--- BlazorLeaflet/BlazorLeaflet/Models/GeoJsonDataLayer.cs
+++ BlazorLeaflet/BlazorLeaflet/Models/GeoJsonDataLayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.JSInterop;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,21 @@ namespace BlazorLeaflet.Models
     public class GeoJsonDataLayer : InteractiveLayer
     {
         public string GeoJsonData { get; set; }
+
+        public bool HasPointToLayerFunc => PointToLayer != null;
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public Func<object, LatLng, Marker> PointToLayer { get; set; }
+
+        [JSInvokable]
+        public object CallPointToLayer(object point, LatLng latLng)
+        {
+            var layer = PointToLayer(point, latLng);
+
+            if (!(layer is Marker))
+                throw new NotSupportedException("Only Marker layers are currently implemented for PointToLayer.");
+
+            return new { Layer = layer, Ref = DotNetObjectReference.Create(layer) };
+        }
     }
 }
