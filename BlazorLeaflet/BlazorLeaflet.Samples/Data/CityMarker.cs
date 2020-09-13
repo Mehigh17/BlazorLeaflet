df1a8c6d6dd9cb53a3bc1b96ef3202756ab8ce82
Max Reble <39700092+maxreb@users.noreply.github.com>
2020-09-13 13:07:00 +0200

    updated to net5.0 and added some removelayer examples

---
+++ BlazorLeaflet/BlazorLeaflet.Samples/Data/CityMarker.cs
@@ -0,0 +1,28 @@
+﻿using BlazorLeaflet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorLeaflet.Samples.Data
+{
+	public class CityMarker : Marker
+	{
+		public CityMarker(City city) : base(city.Coordinates)
+		{
+			Icon = new Icon
+			{
+				Url = city.CoatOfArmsImageUrl,
+				ClassName = "map-icon",
+			};
+			Tooltip = new Tooltip
+			{
+				Content = city.Name,
+			};
+			Popup = new Popup
+			{
+				Content = city.Description,
+			};
+		}
+	}
+}
