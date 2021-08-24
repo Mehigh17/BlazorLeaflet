c468443e982abdea7dae8c2875959d04309ce786
MTimoshinin <m.timoshinin@systtech.ru>
2021-08-24 15:49:38 +0200

    feature: add getBounds

--- BlazorLeaflet/BlazorLeaflet/LeafletInterops.cs
+++ BlazorLeaflet/BlazorLeaflet/LeafletInterops.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Threading.Tasks;
 using Rectangle = BlazorLeaflet.Models.Rectangle;
@@ -85,6 +84,9 @@ namespace BlazorLeaflet
 
         public static ValueTask<float> GetZoom(IJSRuntime jsRuntime, string mapId) =>
             jsRuntime.InvokeAsync<float>($"{_BaseObjectContainer}.getZoom", mapId);
+
+        public static ValueTask<LatLngBounds> GetBounds(IJSRuntime jsRuntime, string mapId) =>
+            jsRuntime.InvokeAsync<LatLngBounds>($"{_BaseObjectContainer}.getBounds", mapId);
 
         public static ValueTask ZoomIn(IJSRuntime jsRuntime, string mapId, MouseEventArgs e) =>
             jsRuntime.InvokeVoidAsync($"{_BaseObjectContainer}.zoomIn", mapId, e);
